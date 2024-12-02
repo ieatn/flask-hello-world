ce4b874a6d890fdc7bdbb03e6d916023bf3c8f22
Dennis Zhang <81874063+ieatn@users.noreply.github.com>
2024-12-02 15:28:35 -0800

    Update index.py

--- api/index.py
+++ api/index.py
@@ -1,6 +1,10 @@
-from flask import Flask
+from flask import Flask, jsonify, request
+from flask_cors import CORS
+import os
+from openai import OpenAI
 
 app = Flask(__name__)
+CORS(app)
 
 @app.route('/')
 def home():
