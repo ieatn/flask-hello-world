73e2e2f524391016d065d7d0f36320141ce185e7
Dennis Zhang <81874063+ieatn@users.noreply.github.com>
2024-12-02 16:39:15 -0800

    Update index.py

--- api/index.py
+++ api/index.py
@@ -1,20 +1,8 @@
-from flask import Flask, jsonify, request
-from flask_cors import CORS
-import os
-from openai import OpenAI
+from flask import Flask
 
 app = Flask(__name__)
-CORS(app)
-client = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
 
 @app.route('/')
 def home():
-    return "Hello, world!"
-
-@app.route('/about')
-def about():
-    return 'About'
-
-
-
+    return 'Hello, World!'
 
